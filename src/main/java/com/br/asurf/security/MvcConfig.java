0c291cc172a55001b0fe133d31af204295bf18e4
thiagobackadelino <thiagobackadelino@gmail.com>
2017-08-13 21:32:40 -0300

    inclusão de template de login security

---
+++ src/main/java/com/br/asurf/security/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.br.asurf.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("/home/home");
+        registry.addViewController("/").setViewName("/home/home");
+        registry.addViewController("/login").setViewName("/login/login");
+    }
+
+}
